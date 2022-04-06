890559aac8143b980c5c5686f7c9245d0e0387ae
AKSHAYBINDU <97433957+AKSHAYBINDU@users.noreply.github.com>
2022-04-06 11:37:27 +0530

    program commited

---
+++ UIsum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class UISum
+{
+public static void main(String[] args)
+{
+   Scanner s1 = new Scanner(System.in);
+   System.out.println("Enter Two Numbers :");
+   int n1, n2,s;
+   n1 = s1.nextInt();
+   n2 = s1.nextInt();
+   s = n1 + n2;
+   System.out.println("Sum of two numbers = " + s);
+  }
+}
